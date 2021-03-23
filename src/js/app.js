bfbe18675e76cb198a6acd071d3aa0681d3e3898
Vladimir Vorobtsov <vnvm5k@yandex.ru>
2021-03-24 00:20:22 +0300

    Done

--- src/js/app.js
+++ src/js/app.js
@@ -1,46 +1,23 @@
 export default function orderByProps(obj, order) {
-  const modifiedArr = Object.entries(obj);
-  const resultArr = [];
-  modifiedArr.forEach((e) => {
-    const key = '';
-    const value = '';
-    const resultObj = { key, value };
-    resultObj.key = e[0];
-    resultObj.value = e[1];
-    resultArr.push(resultObj);
-  });
-
+  const arr = [];
   const orderArr = [];
-  order.forEach((e) => {
-    const value = resultArr.find((element) => element.key === e);
-    orderArr.push(value);
-  });
-
-  const spliceArr = resultArr.slice();
-
-  resultArr.forEach((item, i) => {
-    for (let j = 0; j < order.length; j++) {
-      if (item.key === order[j]) {
-        if (j === 1) {
-          i -= 1;
-        }
-        spliceArr.splice(i, 1);
-      }
+  for (const [key, value] of Object.entries(obj)) {
+    if (key !== order[0] && key !== order[1]) {
+      arr.push({ key, value });
+    } else {
+      orderArr.push({ key, value });
     }
-  });
+  }
 
-
-  spliceArr.sort((a, b) => {
-    const keyA = a.key; 
+  arr.sort((a, b) => {
+    const keyA = a.key;
     const keyB = b.key;
     if (keyA < keyB) { return -1; }
     if (keyA > keyB) { return 1; }
     return 0;
   });
 
+  const result = orderArr.concat(arr);
 
-  for (let j = 1; (j < orderArr.length && j >= 0); j--) {
-    spliceArr.unshift(orderArr[j]);
-  }
-  return spliceArr;
+  return result;
 }
